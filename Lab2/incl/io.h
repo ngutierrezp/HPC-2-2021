67aeb335abd39c4b0e0a8765b0010b62fb0fcd75
isiabasolof <70967286+isiabasolof@users.noreply.github.com>
2021-10-10 17:04:08 -0300

    Update io.h

--- Lab2/incl/io.h
+++ Lab2/incl/io.h
@@ -3,10 +3,10 @@
 
 
 /*
-Descripción:    Función que lee un archivo .txt y genera una matriz de datos de particulas
+Descripción:    Función que lee un archivo .txt y genera una matriz de datos de partículas
                 De la forma: 
 
-                    posición impacto  | Energia de impacto
+                    Posición Impacto  | Energía de impacto
                             |                   |
                             v                   v
 
@@ -14,32 +14,32 @@ Descripción:    Función que lee un archivo .txt y genera una matriz de datos d
                             23                  234
                             ...                 ...
 
-                La posición de impacto corresponde al indice 0 y la energia al indice 1.
+                La posición de impacto corresponde al índice 0 y la energía al índice 1.
 
-parametro PATH_FILE: Es un string que representa la dirección o nombre de un archivo .txt
+Parámetro PATH_FILE: Es un string que representa la dirección o nombre de un archivo .txt. 
 
-parametro particles: corresponde a un valor de memoria donde se almacenaran la cantidad de 
-                    Particulas leidas en el archivo.
+Parámetro Particles: corresponde a un valor de memoria donde se almacenaran la cantidad de 
+                    Partículas leídas en el archivo.
 
-Salida:  Devuelve una matriz de datos de particulas donde la columna 0 es la posición de impacto
-        y la columna 1 es la energia de impacto.
+Salida:  Devuelve una matriz de datos de partículas donde la columna 0 es la posición de impacto
+        y la columna 1 es la energía de impacto.
 */
 int **read_file(char *PATH_FILE, int *particles);
 
 /*
 Descripción:    Función que genera un archivo que contiene la información listada de 
-                las posiciones del material impactado y su energia.
+                las posiciones del material impactado y su energía.
 
-parametro PATH_FILE: Es un string que representa la dirección o nombre de un archivo .txt
+Parámetro PATH_FILE: Es un string que representa la dirección o nombre de un archivo .txt. 
 
-parametro vector:   Corresponde al vector de energias que representa al material donde
-                    impactaron las particulas.
+Parámetro vector:   Corresponde al vector de energías que representa al material donde
+                    impactaron las partículas. 
 
-parametro N: Corresponde a las divisiones del elemento en 1D. Es el largo de este elemento.
+Parámetro N: Corresponde a las divisiones del elemento en 1D. Es el largo de este elemento.
 
-parametro index: corresponde a la posición del elemento de mayor energia en el vector de energias.
+Parámetro Index: corresponde a la posición del elemento de mayor energía en el vector de energías.
 
-Salida:  No posee salidas pero genera un archivo con el listado de posiciones y enegia en esas posiciones.
+Salida:  No posee salidas pero genera un archivo con el listado de posiciones y energía en esas posiciones.
 */
 void write_file(char *PATH_FILE, int N, float *vector, int index);
 #endif
