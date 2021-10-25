d9c9e2ba1a0491065aa14c0e31dff4ca7b843875
Nicolas Gutierrez <nicolas@ngutierrezp.cl>
2021-10-24 22:30:11 -0300

    FIX: Shared memory

--- Lab3/hist.cu
+++ Lab3/hist.cu
@@ -10,13 +10,19 @@
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 
+/*
+    ############ IMPORTANTE ############
+    Leer el apartado de importancia en el readme.md
+*/
+
 //Función de suma en cuda con operación atomica.
 __global__ void histogram(int *I, int n, int m, int *H, int Q)
 {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
-    printf(" mi id: %d\n", id);
+    int value = I[id];
 
-    int value = H[id];
+    // Se obtiene el valor de la imagen en la posición id
+    printf("mi id:\t%d - value :\t%d  \n", id, (int)value);
 
     if (id < n * m)
     {
@@ -27,25 +33,38 @@ __global__ void histogram(int *I, int n, int m, int *H, int Q)
     }
 }
 
-// Función de suma en cuda con operación atomica con memoria compartida.
-// __global__ void histogram_shared( int *I, int n, int m,  int *H, int Q)
-// {
-//     int id = blockIdx.x * blockDim.x + threadIdx.x;
+//Función que crea el histograma en cuda con memoria compartida.
+__global__ void histogram_shared(int *I, int n, int m, int *H, int Q)
+{
+    #define BLOCK_SIZE 256
+    __shared__ int shared[BLOCK_SIZE];
 
-//      int value = I[id];
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
 
-//     // define shared memory
-//     __shared__  int temp[shared[Q]];
+    // Se inicializ el vector de memoria compartida en 0.
+    if (threadIdx.x < n*m)
+    {
+        shared[threadIdx.x] = 0;
+    }
+    //Barrera
+    __syncthreads();
+    if (id < Q)
+    {
+        int value = I[id];
+        atomicAdd(&shared[value], 1);
+    }
+       //Barrera
+    __syncthreads();
 
-//     if (id < n)
-//     {
-//         if(value < Q)
-//         {
-//             atomicAdd(&shared[value], 1);
-//         }
+    if (threadIdx.x < n*m)
+    {
+        atomicAdd(&H[threadIdx.x], shared[threadIdx.x]);
+    }
 
-//     }
-// }
+    
+    
+    
+}
 
 int main(int argc, char *argv[])
 {
@@ -63,6 +82,7 @@ int main(int argc, char *argv[])
     char *i = (char *)malloc(sizeof(char) * _MAX_STRING_SIZE);
     char *o = (char *)malloc(sizeof(char) * _MAX_STRING_SIZE);
     int opt;
+    cudaError_t status;
     int Q = 256; // Se define Q por defecto para escala de grisis (0,255)
     if (argc != 13)
     {
@@ -97,6 +117,13 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
     }
+    int *driver = (int *)malloc(sizeof(int));
+    int *runtime = (int *)malloc(sizeof(int));
+
+    status = cudaRuntimeGetVersion(runtime);
+    printf("%s \n", cudaGetErrorString(status));
+    status = cudaDriverGetVersion(driver);
+    printf("%s \n", cudaGetErrorString(status));
 
     if (debug)
     {
@@ -106,46 +133,49 @@ int main(int argc, char *argv[])
         printf(GRN "Archivo de salida: " reset "%s\n", o);
         printf(GRN "Numero de hebras: " reset "%d\n", t);
         printf(GRN "Debug: " reset "%d\n", debug);
+        printf(GRN "Driver Version: " reset "%d\n", *driver);
+        printf(GRN "Runtime Version: " reset "%d\n", *runtime);
         printf("\n\n");
     }
 
-    
     int *histo_cuda;
+    int *histo_cuda_shared;
     int *image_cuda;
 
     int *image = read_raw(i, m, n); // lectura de la imagen de entrada.
     int *histo = (int *)malloc(sizeof(int) * Q);
+    int *histo_shared = (int *)malloc(sizeof(int) * Q);
     // Se inicializa el array con 0.
     for (int i = 0; i < Q; i++)
     {
         histo[i] = 0;
     }
-    
-    
 
     cudaMalloc((void **)&histo_cuda, Q * sizeof(int));
+    cudaMalloc((void **)&histo_cuda_shared, Q * sizeof(int));
     cudaMalloc((void **)&image_cuda, m * n * sizeof(int));
 
     // Se copia la imagen de entrada a la memoria en cuda.
     cudaMemcpy(image_cuda, image, m * n * sizeof(int), cudaMemcpyHostToDevice);
-    // Se copia la memoria de hisograma a la memoria en cuda.
     cudaMemcpy(histo_cuda, histo, Q * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(histo_cuda_shared, histo_shared, Q * sizeof(int), cudaMemcpyHostToDevice);
 
     // Se llama el kernel de histograma con operación atomica.
     histogram<<<(int)ceil((m * n) / t), t>>>(image_cuda, m, n, histo_cuda, Q);
+    histogram_shared<<<(int)ceil((m * n) / t), t>>>(image_cuda, m, n, histo_cuda_shared, Q);
     cudaDeviceSynchronize();
     // Se copia la memoria de histograma a la memoria en host.
     cudaMemcpy(histo, histo_cuda, Q * sizeof(int), cudaMemcpyDeviceToHost);
-    
-    
+    cudaMemcpy(histo_shared, histo_cuda_shared, Q * sizeof(int), cudaMemcpyDeviceToHost);
+
     // Print de histograma en host.
     if (debug)
     {
         printf(GRN "Histograma en host:\n" reset);
-        printf("Valor\t\tHisto\t\tHisto_shared\n");
+        printf("Valor\tHisto\tHisto_shared\n");
         for (int i = 0; i < Q; i++)
         {
-            printf("%d\t->\t%d\n", i, histo[i]);
+            printf("%d\t->\t%d\t%d\n", i, histo[i],histo_shared[i]);
         }
         printf("\n\n");
     }
@@ -158,6 +188,8 @@ int main(int argc, char *argv[])
     free(o);
     free(image);
     free(histo);
+    free(driver);
+    free(runtime);
     cudaFree(histo_cuda);
     cudaFree(image_cuda);
 
